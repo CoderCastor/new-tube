c885730373615183bb953d2302cfc739796cfb69
Tejas Shinde <tejasshinde@Tejass-MacBook-Pro.local>
2025-07-09 19:27:55 +0530

    checkpoint 7 - trpc config | rate limit | redis

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -3,17 +3,14 @@ import { ErrorBoundary } from "react-error-boundary";
 import { Suspense } from "react";
 import { PageClient } from "./client";
 
-
-export default async function Home () {
-
-
+export default async function Home() {
   return (
     <HydrateClient>
       <Suspense fallback={<p>Loading...</p>}>
-        <ErrorBoundary fallback={<p>Error...</p>}>
+        <ErrorBoundary fallback={<p>Error</p>}>
           <PageClient />
         </ErrorBoundary>
       </Suspense>
     </HydrateClient>
-  )
+  );
 }
